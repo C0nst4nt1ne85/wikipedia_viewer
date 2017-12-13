99f958c9e2f44228ec6d6f626256b6b97f47494b
Constantine Melios <djagil@gmail.com>
2017-12-13 13:40:31 +0200

    End

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-$(document).ready(function () {
+//Get random wiki
+function random(){
+    window.open("https://en.wikipedia.org/wiki/Special:Random"); 
+}
 
+//search and display content and nav
+function wikipedia(search, place){
+    //retrieve data
+    $.ajax({
+        url: `http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=${search}&gsrlimit=50&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&formatversion=2&origin=*`
+    }).done(function (data) {
+        var html = "";
+        //display nav
+        $("#moreBut").css("display", "inline-block");
+        if (place == 0) {
+            $("#prevBut").css("display", "none");
+        } else {
+            $("#prevBut").css("display", "inline-block");
+        }
+        //display data
+        for (var i = place; i - place < 10; i++) {
+            var title = data.query.pages[i].title;
+            var extract = data.query.pages[i].extract;
+            html += `<a href="https://en.wikipedia.org/wiki/${title}"><h4>${title}</h4>
+        <p>${extract}</p></a>`;
+        }
+        $("#main").html(html);
+    })
+}
+
+
+
+$(document).ready(function () {
+var place = 0;
+var searchTerm ="";
+
+//input
     $("#search").keydown(function(input) {
         if (input.keyCode == 13){
-            var searchTerm = $("#search").val();
+            searchTerm = $("#search").val();
             input.preventDefault();
+            place = 0;
             if (searchTerm == "") {
-                window.open("https://en.wikipedia.org/wiki/Special:Random")
+                random();
             } else {
-                console.log(searchTerm);
+                wikipedia(searchTerm, place);
             }
+            searchTerm = searchTerm;
         }
-    });
 
+    })
+//buttons
+    $("#random").click(function(){
+        random();
+    })
+
+    $("#moreBut").click(function () {
+        place += 10;
+        wikipedia(searchTerm, place);
+    })
+
+    $("#prevBut").click(function () {
+        place -= 10;
+        wikipedia(searchTerm, place);
+    })
 });
+
